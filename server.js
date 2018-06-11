fa5fba6ef9446abc946baaf917ef60de85ff2089
Dan Klinga <dklinga@Dans-MacBook-Pro.local>
2018-06-11 18:12:02 -0400

    monday3

--- server.js
+++ server.js
@@ -38,17 +38,15 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/week18Populater");
-
+//mongoose.connect("mongodb://localhost/week18Populater");
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-//var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/week18Populater";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-//mongoose.Promise = Promise;
-//mongoose.connect(MONGODB_URI);
-
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
